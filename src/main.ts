0abf74a2949ad6866a9b1514e23d2b0811f75f21
Janne Vikstedt <vikstedt.janne@gmail.com>
2020-07-10 14:08:35 +0300

    Google authentication

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import i18n from "./i18n";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import GSignInButton from "vue-google-signin-button";
+
 import "jsoneditor/dist/jsoneditor.min.css";
+
+Vue.use(GSignInButton);
 
 Vue.config.productionTip = false;
 
