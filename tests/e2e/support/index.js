c46446ac39ea80a52524c9a531581d085e8258b8
Janne Vikstedt <vikstedt.janne@gmail.com>
2020-07-23 10:52:07 +0300

    Created e2e tests for searching

--- tests/e2e/support/index.js
+++ tests/e2e/support/index.js
@@ -13,6 +13,8 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+import "cypress-wait-until";
+
 // Import commands.js using ES2015 syntax:
 import "./commands";
 
