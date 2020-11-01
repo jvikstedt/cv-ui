f0ab2d6323c1b3f59ae1c5ddbc4b269d1f1e6e4e
Janne Vikstedt <vikstedt.janne@gmail.com>
2020-11-01 15:22:07 +0200

    User create and search

--- src/services/playground/user.ts
+++ src/services/playground/user.ts
@@ -1,8 +1,38 @@
+import * as R from "ramda";
 import ApiService from "@/services/api/user";
-import UserModule from "@/store/modules/user";
-import { PatchUserDto } from "../user";
+import UserModule, { User } from "@/store/modules/user";
+import { SortArrayOfNumbers } from "@/helpers";
+import {
+  CreateUserDto,
+  PatchUserDto,
+  SearchUserDto,
+  UserSearchResult,
+} from "../user";
 
 export default class UserService extends ApiService {
+  private fakedUsers: User[] = [];
+
+  public async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const id =
+      (R.defaultTo(
+        0,
+        R.last(SortArrayOfNumbers(UserModule.allIds))
+      ) as number) + 1;
+
+    const user = {
+      ...createUserDto,
+      id,
+      username: createUserDto.email,
+      avatarId: "",
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    this.fakedUsers.push(user);
+    await UserModule.saveUsers([user]);
+    return user;
+  }
+
   public async patchUser({ id, data }: PatchUserDto): Promise<void> {
     const originalUser = UserModule.find(id);
     if (!originalUser) {
@@ -14,5 +44,18 @@ export default class UserService extends ApiService {
     };
 
     UserModule.add([user]);
+  }
+
+  public async searchUsers(
+    searchUserDto: SearchUserDto
+  ): Promise<UserSearchResult> {
+    const result = await super.searchUsers(searchUserDto);
+
+    const { items, total } = result;
+
+    return {
+      items: [...items, ...this.fakedUsers],
+      total: total + R.length(this.fakedUsers),
+    };
   }
 }
